371a7f52f3afde60b6c11aba65f24234b3966bcd
Siddhant <siddhantgupta792000@gmail.com>
2021-11-13 01:48:06 +0530

    Secret Key Fixes

--- PoodleBE/settings.py
+++ PoodleBE/settings.py
@@ -19,11 +19,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(j+-n0*l0l11)z@cm%k^5cxtn_x=d-v5(9pg$!$lkcauv$z_!t'
-
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
