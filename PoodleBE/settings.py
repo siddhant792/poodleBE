b1c34633bc7d9ff7c6008f74ec3a94578cc81104
Siddhant <siddhantgupta792000@gmail.com>
2021-11-14 23:28:17 +0530

    CORS

--- PoodleBE/settings.py
+++ PoodleBE/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'rest_framework.authtoken',
     'apps.accounts.app_config.AccountsConfig',
     'apps.services.app_config.ServicesConfig',
@@ -48,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'PoodleBE.urls'
